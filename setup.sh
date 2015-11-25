90b67ff879f4d107bcb51a2d68e20d8484cb104a
Keegan Bowen <keegan.bowen@gmail.com>
2015-11-24 21:02:31 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,13 @@
 echo "What is your username?";
 read SETUPUSER
 
-mkdir /home/"$SETUPUER"/archive-spider;
+mkdir /home/"$SETUPUSER"/archive-spider
+mkdir /home/"$SETUPUSER"/tmp
+
+echo "Now installing netstew.py to /usr/local/scripts"
+mkdir -P /usr/local/scripts
+cp ./netstew.py /usr/local/scripts/netstew.py
+
+echo "Now installing archive-spider to /usr/local/bin/archive-spider"
+cp ./archive-spider.sh /usr/local/bin/
+chmod +x /usr/local/bin/archive-spider
