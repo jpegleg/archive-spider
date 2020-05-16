3bd6a160971474afa9ba105990af82b54221a209
Keegan Bowen <keegan.bowen@gmail.com>
2020-05-16 00:28:21 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,8 @@ cp ./netstew.py /usr/local/scripts/netstew.py
 
 echo "Now installing archive-spider to /usr/local/bin/archive-spider"
 cp ./archive-spider.sh /usr/local/bin/archive-spider
-chmod +x /usr/local/bin/archive-spider
+chmod +x /usr/local/bin/archive-spider /usr/local/scripts/netstew.py
+
+chown "$SETUPUSER":"$SETUPUSER" /home/"$SETUPUSER"/archive-spider/tmp /usr/local/scripts/netstew.py
+
+echo "Install python module bs4 if you haven't. Alternatively, replace /usr/local/scripts/netstew.py with something else."
